e0e474ae44674f57d909dca55d62103f8a6a10e3
Monique Azevedo <monique.silva@maxmilhas.com.br>
2020-04-13 14:16:43 -0300

    Implementa modules e refatora codigo

--- aulas/primeiro-projeto-node/src/routes/appointments.routes.ts
+++ aulas/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,22 +1,35 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentRoutes = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
-appointmentRoutes.post('/', (resquest, response) => {
-  const { provider, date } = resquest.body;
+appointmentRoutes.post('/', (request, response) => {
+  const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSamDate = appointments.find((appointment) => {
+    return isEqual(parseDate, appointment.date);
+  });
+
+  if (findAppointmentInSamDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = new Appointment(provider, parseDate);
 
   appointments.push(appointment);
 
   return response.json(appointment);
+});
+
+appointmentRoutes.get('/', (request, response) => {
+  response.status(200).json(appointments);
 });
 
 export default appointmentRoutes;
